cdcacac77a16232b5836e2072f78d6ff5886e1c7
Dorothy Zhang <d.l.zhang@student.tudelft.nl>
2022-02-02 01:08:54 +0100

    2021.01.1

---
+++ 2021/Day1/First/main.py
@@ -0,0 +1,14 @@
+f = open("input.txt")
+lines = f.readlines()
+prev = current = int(lines[0])
+increase = 0
+
+for line in lines:
+    prev = current
+    current = int(line)
+    if current > prev:
+        increase = increase + 1
+
+f.close()
+
+print(increase)
