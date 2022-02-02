9318474a6801759ad8204f17c372a9533e8f32ed
Dorothy Zhang <d.l.zhang@student.tudelft.nl>
2022-02-02 01:33:06 +0100

    2021.01.2

---
+++ 2021/Day1/Second/main.py
@@ -0,0 +1,20 @@
+f = open("../input.txt")
+queue = [int(f.readline()), int(f.readline()), int(f.readline())]
+prevSum = currentSum = sum(queue)
+count = 0
+
+while True:
+    line = f.readline()
+    if not line:
+        break
+    prevSum = currentSum
+    queue.pop(0)
+    queue.append(int(line))
+    currentSum = sum(queue)
+
+    if currentSum > prevSum:
+        count += 1
+
+f.close()
+
+print(count)
