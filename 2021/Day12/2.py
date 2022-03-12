2c30b9241e8cd89df651a107151e0279347abd36
riceisacereal <zhangdorothylele@gmail.com>
2022-03-12 20:25:01 +0100

    Added alternative methods that I thought would make things a lot faster but it's not significant

--- 2021/Day12/2.py
+++ 2021/Day12/2.py
@@ -1,3 +1,4 @@
+import time
 paths = 0
 ORD_LOWER_A = 97
 
@@ -5,6 +6,26 @@ ORD_LOWER_A = 97
 def incr():
     global paths
     paths += 1
+
+
+def can_visit_alt(e, twice_chance, small_caves):
+    """
+    return:
+        0 - can not visit
+        1 - can visit
+        2 - can visit, second small cave visit
+    """
+    if ord(e[0]) >= ORD_LOWER_A and e in small_caves:  # is small cave and in path
+        if twice_chance:
+            # has a cave that's been visited twice
+            # a new cave is visited twice
+            # or cave that is visited twice is visited a third time
+            return 0
+        else:
+            # use second chance
+            return 2
+
+    return 1
 
 
 def can_visit(path, e, twice_chance):
@@ -25,6 +46,28 @@ def can_visit(path, e, twice_chance):
             return 2
 
     return 1
+
+
+def count_paths_alt(dic, path, e, twice_chance, small_caves):
+    if e == "end":  # finish a path
+        incr()
+        return
+
+    path.append(e)
+    is_small = ord(e[0]) >= ORD_LOWER_A
+    if is_small:
+        small_caves.append(e)
+
+    edges = dic.get(e)
+    for edge in edges:
+        ind = can_visit_alt(edge, twice_chance, small_caves)
+        if ind:
+            count_paths_alt(dic, path, edge, twice_chance or (ind == 2), small_caves)
+
+    if is_small:
+        small_caves.pop()
+
+    path.pop()
 
 
 def count_paths(dic, path, e, twice_chance):
@@ -82,8 +125,13 @@ def main(file_name):
     print(cave)
 
     # count paths
+    time1 = time.time()
     count_paths(cave, [], "start", False)
-    print("Total paths:", paths)
+    time2 = time.time()
+    count_paths_alt(cave, [], "start", False, [])
+    time3 = time.time()
+    print("Time method 1:", time2 - time1)
+    print("Time method 2:", time3 - time2)
 
 
 main("input.txt")
