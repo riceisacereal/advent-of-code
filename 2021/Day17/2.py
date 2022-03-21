5dd17b8228b2694527498c8bec0ae36918117f1e
riceisacereal <zhangdorothylele@gmail.com>
2022-03-21 23:06:40 +0100

    Working solution

--- 2021/Day17/2.py
+++ 2021/Day17/2.py
@@ -1,16 +1,24 @@
-import math
+def intersects(key_x, value_x, value_y, stops_in_range):
+    for step_y in value_y:
+        if step_y >= key_x and key_x in stops_in_range:
+            """probe has stopped in x distance at this point"""
+            return True
+        elif step_y in value_x:
+            """has matching step"""
+            return True
+
+    return False
 
 
-def intersects(lx, ly, key_x, key_y):
-    pass
-
-
-def get_distinct_velocities(x_velocities, y_velocities):
+def get_distinct_velocities(x_velocities, y_velocities, stops_in_range):
     total = 0
-    for (key_y, value_y) in y_velocities:
-        for (key_x, value_x) in x_velocities:
-            if intersects():
+    for (key_y, value_y) in y_velocities.items():
+        for (key_x, value_x) in x_velocities.items():
+            if intersects(key_x, value_x, value_y, stops_in_range):
+                # print(key_x, ",", key_y, sep='')
                 total += 1
+
+    return total
 
 
 def add_y_velocities(y_range, y_velocities):
@@ -28,20 +36,20 @@ def add_y_velocities(y_range, y_velocities):
                 steps = start_v - v + 1
                 """add to negative"""
                 if y_velocities.get(start_v):
-                    y_velocities[start_v].append(steps)
+                    y_velocities[start_v].add(steps)
                 else:
-                    y_velocities[start_v] = [steps]
+                    y_velocities[start_v] = {steps}
                 """add to positive"""
                 pos_v = (-start_v) - 1
                 if y_velocities.get(pos_v):
-                    y_velocities[pos_v].append(2 * pos_v + steps)
+                    y_velocities[pos_v].add(2 * pos_v + 1 + steps)
                 else:
-                    y_velocities[pos_v] = [2 * pos_v + steps]
+                    y_velocities[pos_v] = {2 * pos_v + 1 + steps}
 
             v -= 1
 
 
-def add_x_velocities(x_range, x_velocities):
+def add_x_velocities(x_range, x_velocities, stops_in_range):
     for start_v in range(1, (x_range[1] // 2) + 2):
         curr_x_distance = 0
         """count down"""
@@ -53,22 +61,26 @@ def add_x_velocities(x_range, x_velocities):
                 break
             elif curr_x_distance >= x_range[0]:
                 """probe is in range"""
+                if v == 1:
+                    """probe stops in range"""
+                    stops_in_range.add(start_v)
+
                 if x_velocities.get(start_v):
                     """add current step number"""
-                    x_velocities[start_v].append(start_v - v + 1)
+                    x_velocities[start_v].add(start_v - v + 1)
                 else:
                     """create entry with current step number"""
-                    x_velocities[start_v] = [start_v - v + 1]
+                    x_velocities[start_v] = {start_v - v + 1}
 
 
 def add_direct_hits(x_range, y_range, x_velocities, y_velocities):
     for velocity in range(x_range[0], x_range[1] + 1):
-        x_velocities[velocity] = [1]
+        x_velocities[velocity] = {1}
     for velocity in range(y_range[0], y_range[1] + 1):
         if velocity < 0:
             pos_v = (-velocity) - 1
-            y_velocities[pos_v] = [2 * pos_v + 2]
-        y_velocities[velocity] = [1]
+            y_velocities[pos_v] = {2 * pos_v + 2}
+        y_velocities[velocity] = {1}
 
 
 def main(file_name):
@@ -79,15 +91,17 @@ def main(file_name):
 
     x_velocities, y_velocities = {}, {}
     """stops in x after step ? = initial velocity"""
+    stops_in_range = set()
     """Add direct hits"""
     add_direct_hits(x_range, y_range, x_velocities, y_velocities)
-    add_x_velocities(x_range, x_velocities)
+    """Checking which x, y velocities fall into the range"""
+    add_x_velocities(x_range, x_velocities, stops_in_range)
     add_y_velocities(y_range, y_velocities)
 
+    # print(x_velocities)
+    # print(y_velocities)
+
+    print("Total distinct velocities:", get_distinct_velocities(x_velocities, y_velocities, stops_in_range))
 
 
-    print(x_velocities)
-    print(y_velocities)
-
-
-main("example.txt")
+main("input.txt")
