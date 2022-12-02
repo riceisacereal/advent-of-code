f81836b116e53022a37e8239c1071809fe0ada45
riceisacereal <zhangdorothylele@gmail.com>
2022-12-02 09:46:16 +0100

    2022.2.2 and remove comments

--- 2022/Day02/1.js
+++ 2022/Day02/1.js
@@ -5,10 +5,7 @@ const lines = input.split(/\r?\n/);
 
 let opponent = ["A", "B", "C"];
 let me = ["X", "Y", "Z"];
-// rock paper scissor
-// 0 > 2, 1 > 0, 2 > 1
-// 1 > 0 > 2 > 1 > 0
-// 0 < 1 < 2 < 0
+
 let totalScore = 0;
 for (let line of lines) {
     if (line.length === 0) continue;
---
+++ 2022/Day02/2.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", "utf-8");
+const lines = input.split(/\r?\n/);
+
+let opponent = ["A", "B", "C"];
+let outcome = ["X", "Y", "Z"];
+
+let totalScore = 0;
+for (let line of lines) {
+    if (line.length === 0) continue;
+
+    let [o, out] = line.split(" ");
+    let c = outcome.indexOf(out)
+    totalScore += c * 3
+
+    let score;
+    if (c === 1) {
+        // draw
+        score = opponent.indexOf(o)
+    } else if (c === 0) {
+        // lose
+        score = (opponent.indexOf(o) + 2) % 3
+    } else {
+        // win
+        score = (opponent.indexOf(o) + 1) % 3
+    }
+
+    totalScore += score + 1
+
+}
+
+console.log("Total score: " + totalScore)
