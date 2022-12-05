a1ad7d950b16d72626e376fc44b45c99a0648143
riceisacereal <zhangdorothylele@gmail.com>
2022-12-05 16:23:50 +0100

    2022.5.2 but I can't fix the code duplication

---
+++ 2022/Day05/2.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", "utf-8");
+const lines = input.split(/\r?\n/);
+
+let rows = []
+let lineIndex = 0;
+// Read all crate lines
+while (lineIndex < lines.length) {
+    let row = lines[lineIndex].split(/[\[\]]/).filter(e => e);
+    if (row.length === 0) {
+        break;
+    }
+    rows.push(row);
+    lineIndex++;
+}
+
+// Populate stacks with empty arrays
+let stacks = rows.pop()[0].match(/\d/g);
+for (let i = 0; i < stacks.length; i++) {
+    stacks[i] = [];
+}
+
+// Push the crates onto the stacks
+while (rows.length > 0) {
+    let row = rows.pop();
+    let stackIndex = 0;
+    for (let e of row) {
+        if (e.length === 1 && e.match(/[A-Z]/)) {
+            stacks[stackIndex].push(e);
+            stackIndex++;
+        } else {
+            stackIndex += Math.floor(e.length / 4);
+        }
+    }
+}
+
+lineIndex++;
+// Move crates
+while (lineIndex < lines.length) {
+    let operation = lines[lineIndex].match(/\d+/g).map(e => parseInt(e));
+    let tempStack = []
+    for (let i = 0; i < operation[0]; i++){
+        tempStack.push(stacks[operation[1] - 1].pop())
+    }
+    while (tempStack.length > 0) {
+        stacks[operation[2] - 1].push(tempStack.pop())
+    }
+    lineIndex++;
+}
+
+console.log(stacks)
+
+let tops = []
+// Get top crate of every stack
+for (let stack of stacks) {
+    tops.push(stack.pop())
+}
+
+console.log(tops.join(""))
