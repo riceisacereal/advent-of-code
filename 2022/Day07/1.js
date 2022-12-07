7ce413dac2109ee085c292c585819782aaa1b099
riceisacereal <zhangdorothylele@gmail.com>
2022-12-07 11:36:19 +0100

    correctly reading files and directories

---
+++ 2022/Day07/1.js
@@ -0,0 +1,91 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("test.txt", "utf-8");
+const lines = input.split(/\r?\n/);
+
+const LIMIT = 100000;
+
+class Dir {
+    constructor(name, parent) {
+        this.name = name;
+        this.parent = parent;
+        this.childDirs = new Set();
+        this.content = new Map();
+    }
+}
+
+function cd(line) {
+    let dir = line.substring(5, line.length);
+    switch (dir) {
+        case "..":
+            currentDir = currentDir.parent;
+            break;
+        case "/":
+            currentDir = root;
+            break;
+        default:
+            for (let childDir of currentDir.childDirs) {
+                if (childDir.name === dir) {
+                    currentDir = childDir;
+                    break;
+                }
+            }
+    }
+}
+
+function addContent(line) {
+    line = lines[++i];
+    while (i < lines.length > 0 && line[0] !== "$") {
+        if (line[0] === "d") {
+            // new directory
+            let name = line.substring(4, line.length);
+            let newDir = new Dir(name, currentDir);
+            currentDir.childDirs.add(newDir);
+        } else {
+            // new file
+            let file = line.split(" ");
+            currentDir.content[file[1]] = parseInt(file[0]);
+        }
+        line = lines[++i];
+    }
+}
+
+// function getSize(dir) {
+//     let sizeOfDir = 0;
+//     for (let fileSize of dir.content.values()) {
+//         sizeOfDir += fileSize;
+//     }
+//
+//     let childDirSize = 0;
+//     for (let childDir of dir.childDirs) {
+//         childDirSize += getSize(childDir);
+//     }
+//     sizeOfDir += childDirSize;
+//
+//     if (sizeOfDir > LIMIT) {
+//
+//     }
+//
+//     return sizeOfDir;
+// }
+
+let root = new Dir("/", null);
+let currentDir = root;
+let i = 0;
+while (i < lines.length) {
+    let line = lines[i];
+    if (line[0] === "$") {
+        switch(line[2]) {
+            case "c":
+                cd(line);
+                i++;
+                break;
+            case "l":
+                addContent(line);
+                break;
+        }
+    }
+}
+
+console.log(root);
+
