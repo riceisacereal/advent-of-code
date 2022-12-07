1d9eb3c17d88f188038b160d790fa3ce99bbf1ae
riceisacereal <zhangdorothylele@gmail.com>
2022-12-07 14:43:05 +0100

    Tidy

--- 2022/Day07/1.js
+++ 2022/Day07/1.js
@@ -10,9 +10,15 @@ class Dir {
         this.name = name;
         this.parent = parent;
         this.childDirs = new Set();
-        this.content = new Map();
+        this.files = new Map();
+    }
+
+    setSize(size) {
+        this.size = size;
     }
 }
+
+const root = new Dir("/", null);
 
 function cd(line) {
     let dir = line.substring(5, line.length);
@@ -33,8 +39,7 @@ function cd(line) {
     }
 }
 
-function addContent(line) {
-    line = lines[++i];
+function addFiles(line) {
     while (i < lines.length > 0 && line[0] !== "$") {
         if (line[0] === "d") {
             // new directory
@@ -44,56 +49,46 @@ function addContent(line) {
         } else {
             // new file
             let file = line.split(" ");
-            currentDir.content.set(file[1], parseInt(file[0]));
+            currentDir.files.set(file[1], parseInt(file[0]));
         }
         line = lines[++i];
     }
 }
 
-function getSize(dir) {
+function calculateSizes(dir) {
+    if (dir.size !== undefined) return dir.size;
+
     let sizeOfDir = 0;
-    for (let fileSize of dir.content.values()) {
+    // get size of its own files
+    for (let fileSize of dir.files.values()) {
         sizeOfDir += fileSize;
     }
-
-    let childDirSize = 0;
+    // get size of child directories
     for (let childDir of dir.childDirs) {
-        childDirSize += getSize(childDir);
+        sizeOfDir += calculateSizes(childDir);
     }
-    sizeOfDir += childDirSize;
 
-    if (sizeOfDir < LIMIT) {
-        console.log("Oversize " + dir.name)
-        overSize.push(sizeOfDir);
-    }
+    if (sizeOfDir < LIMIT) underLimit += sizeOfDir;
 
     return sizeOfDir;
 }
 
-let root = new Dir("/", null);
-let currentDir = root;
-let overSize = [];
+let currentDir;
 let i = 0;
 while (i < lines.length) {
     let line = lines[i];
-    if (line[0] === "$") {
-        switch(line[2]) {
-            case "c":
-                cd(line);
-                i++;
-                break;
-            case "l":
-                addContent(line);
-                break;
-        }
+    switch(line[2]) {
+        case "c":
+            cd(line);
+            i++;
+            break;
+        case "l":
+            line = lines[++i];
+            addFiles(line);
+            break;
     }
 }
 
-console.log(root);
-
-getSize(root);
-let totalOverSize = 0;
-for (let size of overSize) {
-    totalOverSize += size;
-}
-console.log("Oversize directories: " + totalOverSize);
+let underLimit = 0;
+calculateSizes(root);
+console.log("Under limit: " + underLimit);
