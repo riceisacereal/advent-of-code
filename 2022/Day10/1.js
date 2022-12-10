7f713e60bd593586b6b3bc6a179f0b2a5ba8da37
riceisacereal <zhangdorothylele@gmail.com>
2022-12-10 12:48:10 +0100

    2022.10.1

--- 2022/Day10/1.js
+++ 2022/Day10/1.js
@@ -3,3 +3,30 @@ const fs = require("fs");
 const input = fs.readFileSync("input.txt", "utf-8");
 const lines = input.split(/\r?\n/);
 
+function addCycle(n) {
+    for (let i = 0; i < n; i++) {
+        if ((cycle - 20) % 40 === 0) {
+            console.log(`Cycle: ${cycle} X: ${x}`);
+            totalSignalStrength += cycle * x;
+        }
+        cycle++;
+    }
+}
+
+let x = 1;
+let cycle = 1;
+let totalSignalStrength = 0;
+for (let line of lines) {
+    switch (line[0]) {
+        case "n":
+            addCycle(1);
+            break;
+        case "a":
+            addCycle(2);
+            let v = parseInt(line.substring(5, line.length));
+            x += v;
+            break;
+    }
+}
+
+console.log(totalSignalStrength)
