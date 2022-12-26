9e460a2f1b14b23717252063b2d1a1186e026797
riceisacereal <zhangdorothylele@gmail.com>
2022-12-26 15:54:41 +0100

    Cleanup comments

--- 2022/Day19/1.js
+++ 2022/Day19/1.js
@@ -3,9 +3,6 @@ const fs = require("fs");
 const input = fs.readFileSync("test.txt", "utf-8");
 const lines = input.split(/\r?\n/);
 
-// const [OREBOT, CLAYBOT, OBSBOT, GEOBOT] = [0, 1, 2, 3];
-// const [ORE, CLAY, OBSIDIAN, GEODE] = [0, 1, 2, 3];
-
 class Blueprint {
     constructor(line) {
         let numbers = line.split(/costs | ore\.| ore and | clay\.| obsidian\./);
@@ -23,16 +20,12 @@ class Backpack {
         this.bots = [1, 0, 0, 0];
         // Ore, Clay, Obsidian, Geode
         this.materials = [0, 0, 0, 0];
-        // this.snapshots = [];
     }
 
     copyBackpack(backpack) {
         this.time = backpack.time;
         this.bots = [...backpack.bots];
         this.materials = [...backpack.materials];
-        // for (let snap of backpack.snapshots) {
-        //     this.snapshots.push([[...snap[0]], [...snap[1]], snap[2]]);
-        // }
     }
 
     buildBot(blueprint, bot) {
@@ -116,8 +109,6 @@ function exploreBuildingSequence(blueprint, backpack, buildBot) {
         backpack.bots[buildBot]++;
     }
 
-    // backpack.snapshots.push([[...backpack.bots], [...backpack.materials], backpack.time]);
-
     if (backpack.time === 24) {
         // Time up, report geode gain
         return backpack.materials[3];
@@ -144,59 +135,10 @@ function exploreBuildingSequence(blueprint, backpack, buildBot) {
         let newBackPack = new Backpack();
         newBackPack.copyBackpack(backpack);
         let geodes = exploreBuildingSequence(blueprint, newBackPack, bot);
-
-        // console.log(geodes);
-
         if (geodes > maxGeodes) maxGeodes = geodes;
     }
 
     return maxGeodes;
-
-
-    // let moreClay = 0;
-    // let moreOre = 0;
-    // while (backpack.time < 24 && (oreTime || clayTime)) {
-    //     backpack.time++;
-    //
-    //     let buildGeoBot = backpack.buildGeoBot(blueprint);
-    //     let buildObsBot = backpack.buildObsBot(blueprint);
-    //     let buildClayBot = backpack.materials[ORE] >= blueprint.clayBot;
-    //     let buildOreBot = backpack.materials[ORE] >= blueprint.oreBot;
-    //
-    //     // Add materials
-    //     backpack.collectMaterials();
-    //
-    //     if (buildGeoBot) backpack.bots[GEOBOT]++;
-    //     if (buildObsBot) backpack.bots[OBSBOT]++;
-    //     // Go through the time steps until you reach a point where you can either buy an ore or clay bot
-    //     if (clayTime || oreTime) {
-    //         if (clayTime && buildClayBot) {
-    //             // build clay bot
-    //             backpack.materials[ORE] -= blueprint.clayBot;
-    //             backpack.bots[CLAYBOT]++;
-    //             clayTime = false;
-    //         } else if (oreTime && buildOreBot) {
-    //             // build ore bot
-    //             backpack.materials[ORE] -= blueprint.oreBot;
-    //             backpack.bots[OREBOT]++;
-    //             oreTime = false;
-    //         }
-    //     }
-    //
-    //     if (!clayTime && !oreTime) {
-    //         // TODO: branch
-    //         // TODO: new backpacks
-    //         let clayBackPack = new Backpack();
-    //         clayBackPack.copyBackpack(backpack);
-    //         let oreBackPack = new Backpack();
-    //         oreBackPack.copyBackpack(backpack);
-    //
-    //         moreClay = moreOreOrMoreClay(blueprint, clayBackPack, true, false);
-    //         moreOre = moreOreOrMoreClay(blueprint, oreBackPack, false, true);
-    //     }
-    // }
-    //
-    // return Math.max(backpack.materials[GEODE], moreClay, moreOre);
 }
 
 function getMaxGeode(blueprint) {
@@ -207,9 +149,6 @@ function getMaxGeode(blueprint) {
         if (geodes > maxGeodes) maxGeodes = geodes;
     }
     return maxGeodes;
-    // let moreClay = moreOreOrMoreClay(blueprint, new Backpack(), true, false);
-    // let moreOre = moreOreOrMoreClay(blueprint, new Backpack(), false, true);
-    // return Math.max(moreClay, moreOre);
 }
 
 function main() {
@@ -221,9 +160,3 @@ function main() {
 }
 
 main();
-// Geodes -> obsidian-collecting robots -> ore
-// Waterproof obsidian-collecting robot -> needs clay
-// clay-collecting robots -> ore
-// ore-collecting robot - 1
-// 1 resource/minute
-// 1 robot/minute
