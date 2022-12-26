e12c69d4c00687e5fa99296909f2c7caafcb8d0d
riceisacereal <zhangdorothylele@gmail.com>
2022-12-26 15:50:24 +0100

    Works for examples

--- 2022/Day19/1.js
+++ 2022/Day19/1.js
@@ -3,14 +3,16 @@ const fs = require("fs");
 const input = fs.readFileSync("test.txt", "utf-8");
 const lines = input.split(/\r?\n/);
 
-const [OREBOT, CLAYBOT, OBSBOT, GEOBOT] = [0, 1, 2, 3];
-const [ORE, CLAY, OBSIDIAN, GEODE] = [0, 1, 2, 3];
+// const [OREBOT, CLAYBOT, OBSBOT, GEOBOT] = [0, 1, 2, 3];
+// const [ORE, CLAY, OBSIDIAN, GEODE] = [0, 1, 2, 3];
 
 class Blueprint {
     constructor(line) {
         let numbers = line.split(/costs | ore\.| ore and | clay\.| obsidian\./);
-        this.requiredMaterials = [parseInt(numbers[1]), parseInt(numbers[3]),
-            [parseInt(numbers[5]), parseInt(numbers[6])], [parseInt(numbers[8]), parseInt(numbers[9])]];
+        this.oreDemand = [parseInt(numbers[1]), parseInt(numbers[3]), parseInt(numbers[5]), parseInt(numbers[8])];
+        this.maxOreSupplyNeeded = Math.max(...this.oreDemand);
+        this.requiredMaterials = [this.oreDemand[0], this.oreDemand[1],
+            [this.oreDemand[2], parseInt(numbers[6])], [this.oreDemand[3], parseInt(numbers[9])]];
     }
 }
 
@@ -21,12 +23,16 @@ class Backpack {
         this.bots = [1, 0, 0, 0];
         // Ore, Clay, Obsidian, Geode
         this.materials = [0, 0, 0, 0];
+        // this.snapshots = [];
     }
 
     copyBackpack(backpack) {
         this.time = backpack.time;
         this.bots = [...backpack.bots];
         this.materials = [...backpack.materials];
+        // for (let snap of backpack.snapshots) {
+        //     this.snapshots.push([[...snap[0]], [...snap[1]], snap[2]]);
+        // }
     }
 
     buildBot(blueprint, bot) {
@@ -50,70 +56,101 @@ class Backpack {
         }
     }
 
-    // buildGeoBot(blueprint) {
-    //     if (this.materials[ORE] >= blueprint.geoBot[0] && this.materials[OBSIDIAN] >= blueprint.geoBot[1]) {
-    //         this.materials[ORE] -= blueprint.geoBot[0];
-    //         this.materials[OBSIDIAN] -= blueprint.geoBot[1];
-    //         return true;
-    //     }
-    //     return false;
-    // }
-    //
-    // buildObsBot(blueprint) {
-    //     if (this.materials[ORE] >= blueprint.obsBot[0] && this.materials[CLAY] >= blueprint.obsBot[1]) {
-    //         this.materials[ORE] -= blueprint.geoBot[0];
-    //         this.materials[CLAY] -= blueprint.geoBot[1];
-    //         return true;
-    //     }
-    //     return false;
-    // }
+    timeNeeded(blueprint, bot) {
+        switch (bot) {
+            case 0:
+            case 1:
+                if (blueprint.requiredMaterials[bot] <= this.materials[0]) {
+                    return 0;
+                }
+                return Math.ceil((blueprint.requiredMaterials[bot] - this.materials[0]) / this.bots[0]) ;
+            case 2:
+            case 3:
+                if (blueprint.requiredMaterials[bot][0] <= this.materials[0] &&
+                    blueprint.requiredMaterials[bot][1] <= this.materials[bot - 1]) {
+                    return 0;
+                }
+                return Math.max(Math.ceil((blueprint.requiredMaterials[bot][0] - this.materials[0]) / this.bots[0]),
+                    Math.ceil((blueprint.requiredMaterials[bot][1] - this.materials[bot - 1]) / this.bots[bot - 1]));
+        }
+    }
+
+    timeSkip(time) {
+        this.time += time;
+        for (let i = 0; i < 4; i++) {
+            this.materials[i] += time * this.bots[i];
+        }
+    }
+
+    timeSkipToEnd(end) {
+        let time = end - this.time;
+        this.timeSkip(time);
+    }
 
     collectMaterials() {
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 4; i++) {
             this.materials[i] += this.bots[i];
+        }
+    }
+
+    enoughSupply(blueprint, bot) {
+        switch (bot) {
+            case 0:
+                return this.bots[bot] >= blueprint.maxOreSupplyNeeded;
+            case 1:
+            case 2:
+                return this.bots[bot] >= blueprint.requiredMaterials[bot + 1][2];
         }
     }
 }
 
 function exploreBuildingSequence(blueprint, backpack, buildBot) {
     // buildBot = 0-3;
-    // TODO: Always build with max priority: geode -> obsidian -> clay -> ore
-    // TODO: If any of obsidian/clay can be built, check with one level higher: obsidian -> geode, clay -> obsidian
-    // *TODO: If ore can be built, check with all three up
-    // TODO: check if it's more beneficial to build bot or to wait
-    // TODO: If for example, can build obsidian, if obsidian 0, build, if obsidian >= 1, calculate geode + obsidian material for:
-    // TODO: Building, not building
+    let timeNeeded = backpack.timeNeeded(blueprint, buildBot);
+    // backpack.snapshots.push([[...backpack.bots], [...backpack.materials], backpack.time]);
+    if (backpack.time + timeNeeded < 24) {
+        backpack.timeSkip(timeNeeded);
+        backpack.time++;
+        backpack.buildBot(blueprint, buildBot);
+        backpack.collectMaterials();
+        backpack.bots[buildBot]++;
+    }
 
-    // // TODO: Every time step: check to build bots first (geode -> obsidian -> clay & ore), then add material
-    // // TODO: While can't build bot, increase time and collect materials
-    // while (backpack.time < 24) {
-    //     backpack.time++;
-    //
-    //     let build = backpack.buildBot(blueprint, buildBot);
-    //
-    //     backpack.collectMaterials();
-    //
-    //     if (build) {
-    //         backpack.bots[buildBot]++;
-    //         break;
-    //     }
-    // }
-    //
-    // if (backpack.time === 24) {
-    //     // Time up, report geode gain
-    //     return backpack.materials[3];
-    // }
-    //
-    // // TODO: Needed bot built, branch into 4 next bots
-    // let maxGeodes = 0;
-    // for (let bot = 0; bot < 4; bot++) {
-    //     let newBackPack = new Backpack();
-    //     newBackPack.copyBackpack(backpack);
-    //     let geodes = exploreBuildingSequence(blueprint, newBackPack, bot);
-    //     if (geodes > maxGeodes) maxGeodes = geodes;
-    // }
-    //
-    // return maxGeodes;
+    // backpack.snapshots.push([[...backpack.bots], [...backpack.materials], backpack.time]);
+
+    if (backpack.time === 24) {
+        // Time up, report geode gain
+        return backpack.materials[3];
+    }
+
+    // TODO: Needed bot built, branch into 4 next bots
+    let maxGeodes = 0;
+    for (let bot = 0; bot < 4; bot++) {
+        // Does not have required materials to build the bot
+        if (bot > 1 && backpack.bots[bot - 1] === 0) continue;
+
+        // Has enough supply for this bot
+        if (backpack.enoughSupply(blueprint, bot)) {
+            continue;
+        }
+
+        if (backpack.time + backpack.timeNeeded(blueprint, bot) >= 24) {
+            // Can't buy more bots, this is the end
+            backpack.timeSkipToEnd(24);
+            if (backpack.materials[3] > maxGeodes) maxGeodes = backpack.materials[3];
+            continue;
+        }
+
+        let newBackPack = new Backpack();
+        newBackPack.copyBackpack(backpack);
+        let geodes = exploreBuildingSequence(blueprint, newBackPack, bot);
+
+        // console.log(geodes);
+
+        if (geodes > maxGeodes) maxGeodes = geodes;
+    }
+
+    return maxGeodes;
 
 
     // let moreClay = 0;
