1edd3b0aac90a81786f159b56225419bd4f0ea4c
riceisacereal <zhangdorothylele@gmail.com>
2023-12-02 11:51:30 +0100

    2023.2.2 wasn't that hard

--- 2023/Day02/PartTwo.java
+++ 2023/Day02/PartTwo.java
@@ -3,10 +3,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class PartTwo {
     public static void main(String[] args) throws IOException {
-        String puzzleInput = "2023/Day02/test.txt";
-        partOne(puzzleInput);
+        String puzzleInput = "2023/Day02/input.txt";
+        partTwo(puzzleInput);
     }
 
     public static Scanner readFile(String fileName) throws FileNotFoundException {
@@ -14,10 +14,10 @@ public class Main {
         return new Scanner(fr);
     }
 
-    public static int gamePossibility(String line) {
-        int redMax = 12;
-        int greenMax = 13;
-        int blueMax = 14;
+    public static int power(String line) {
+        int redMax = 0;
+        int greenMax = 0;
+        int blueMax = 0;
 
         String[] game = line.split(": ");
         int gameID = Integer.parseInt(game[0].substring(5));
@@ -28,35 +28,29 @@ public class Main {
                 int amount = Integer.parseInt(cubes[0]);
 
                 switch (cubes[1]) {
-                    case "red":
-                        if (amount > redMax) return 0;
-                        break;
-                    case "green":
-                        if (amount > greenMax) return 0;
-                        break;
-                    case "blue":
-                        if (amount > blueMax) return 0;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Could not identify: " + cubes[1]);
+                    case "red" -> redMax = Integer.max(redMax, amount);
+                    case "green" -> greenMax = Integer.max(greenMax, amount);
+                    case "blue" -> blueMax = Integer.max(blueMax, amount);
+                    default -> throw new IllegalArgumentException(
+                        "Could not identify: " + cubes[1]);
                 }
             }
         }
 
-        return gameID;
+        return redMax * greenMax * blueMax;
     }
 
     public static int parseInput(Scanner file) {
         int total = 0;
         while(file.hasNextLine()){
             String line = file.nextLine();
-            total += gamePossibility(line);
+            total += power(line);
         }
 
         return total;
     }
 
-    public static void partOne(String puzzleInput) throws IOException {
+    public static void partTwo(String puzzleInput) throws IOException {
         Scanner file = readFile(puzzleInput);
         int result = parseInput(file);
         System.out.println(result);
