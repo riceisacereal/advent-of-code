81dd28c883a8d5e4e2a8b7cdcd00c71e952c7306
riceisacereal <zhangdorothylele@gmail.com>
2023-12-03 12:16:01 +0100

    2023.3.2 AAAAAAAAAAAAA

--- 2023/Day03/PartTwo.java
+++ 2023/Day03/PartTwo.java
@@ -4,17 +4,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-// 505664 too high
-// 497027 too low
+public class PartTwo {
+    private static int[][] gears;
 
-public class PartOne {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         String puzzleInput = "2023/Day03/input.txt";
         partOne(puzzleInput);
     }
 
     public static List<String> readFile(String fileName) throws IOException {
         return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+    }
+
+    public static Boolean isNumeric(int n) {
+        return (n > 47 && n < 58);
     }
 
     public static Boolean isSymbol(int c) {
@@ -24,39 +27,57 @@ public class PartOne {
             (c > 122 && c < 127);
     }
 
-    public static int checkInclusion(List<String> lines, String line, int level, int start, int end,
-                                     int maxY, int maxX) {
-        int num = Integer.parseInt(line.substring(start, end));
+    public static int checkSecondNumber(int y, int x, int num)
+        throws Exception {
+        int existing = gears[y][x];
+        if (existing == 0) {
+            gears[y][x] = num;
+            return 0;
+        } else if (existing > 0) {
+            gears[y][x] = -(existing * num);
+            return existing * num;
+        } else {
+            throw new Exception("More than two adjacent detected");
+//            return existing;
+        }
+    }
+
+    public static int checkInclusion(List<String> lines, int level, int start, int end)
+        throws Exception {
+        String line = lines.get(level);
+        int maxX = line.length();
+        int maxY = lines.size();
+
+        int num = Integer.parseInt(lines.get(level).substring(start, end));
         for (int y = Math.max(level - 1, 0); y < Math.min(level + 2, maxY); y++) {
             for (int x = Math.max(start - 1, 0); x < Math.min(end + 1, maxX); x++) {
                 int c = lines.get(y).charAt(x);
-                if (isSymbol(c)) {
-//                    System.out.println(num);
-                    return num;
+                if (c == 42) {
+                    return checkSecondNumber(y, x, num);
                 }
             }
         }
         return 0;
     }
 
-    public static int parseInput(List<String> lines) {
+    public static int parseInput(List<String> lines) throws Exception {
+        gears = new int[lines.size()][lines.get(0).length()];
+
         int sumPartNum = 0;
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
             for (int j = 0; j < line.length(); j++) {
                 char c = line.charAt(j);
-                if ((int) c > 47 && (int) c < 58) {
+                if (isNumeric(c)) {
                     int start = j;
                     for (; j < line.length(); j++) {
                         c = line.charAt(j);
                         if (c == '.' || isSymbol(c)) {
-                            sumPartNum += checkInclusion(lines, line, i, start, j, lines.size(),
-                                line.length());
+                            sumPartNum += checkInclusion(lines, i, start, j);
                             break;
                         }
                         if (j + 1 == line.length()) {
-                            sumPartNum += checkInclusion(lines, line, i, start, line.length(),
-                                lines.size(), line.length());
+                            sumPartNum += checkInclusion(lines, i, start, line.length());
                         }
                     }
                 }
@@ -65,7 +86,7 @@ public class PartOne {
         return sumPartNum;
     }
 
-    public static void partOne(String puzzleInput) throws IOException {
+    public static void partOne(String puzzleInput) throws Exception {
         List<String> lines = readFile(puzzleInput);
         int result = parseInput(lines);
         System.out.println(result);
