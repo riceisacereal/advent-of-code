21ff4772eb2dc49781ad7a944c80da409c90c618
riceisacereal <zhangdorothylele@gmail.com>
2023-12-04 13:34:53 +0100

    Improve code structure, use int comparison instead of String

--- 2023/Day04/PartTwo.java
+++ 2023/Day04/PartTwo.java
@@ -2,60 +2,68 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class PartTwo {
-    private static int[] wins;
-    private static int[] copies;
+    private static final String puzzleInput = "2023/Day04/input.txt";
 
     public static void main(String[] args) throws IOException {
-        String puzzleInput = "2023/Day04/input.txt";
-        partTwo(puzzleInput);
+        List<String> lines = readFile(puzzleInput);
+
+        int numCards = lines.size();
+        int[] wins = new int[numCards];
+        int[] copies = new int[numCards];
+        for (int i = 0; i < numCards; i++) {
+            copies[i] = 1;
+        }
+        calculateWins(lines, wins);
+        calculateCopies(wins, copies);
+        int result = sumCopies(copies);
+        System.out.println(result);
     }
 
     public static List<String> readFile(String fileName) throws IOException {
         return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
     }
 
-    public static int parseInput(List<String> lines) {
+    public static void calculateWins(List<String> lines, int[] wins) {
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
+
             String[] card = line.split(": ")[1].split(" \\| ");
-            String[] winNums = card[0].trim().split(" +");
-            String[] ownNums = card[1].trim().split(" +");
-            for (String o : ownNums) {
-                for (String w : winNums) {
-                    if (Objects.equals(w, o)) { // CHANGE TO INT
+            int[] winNums = Arrays.stream(card[0].trim().split(" +"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+            int[] ownNums = Arrays.stream(card[1].trim().split(" +"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+            for (int o : ownNums) {
+                for (int w : winNums) {
+                    if (o == w) {
                         wins[i]++;
+                        break;
                     }
                 }
             }
         }
+    }
 
-        for (int i = 0; i < lines.size(); i++) {
+    public static void calculateCopies(int[] wins, int[] copies) {
+        int size = wins.length;
+        for (int i = 0; i < size; i++) {
             int win = wins[i];
-            for (int j = i + 1; j < Math.min(i + win + 1, lines.size()); j++) {
+            for (int j = i + 1; j < Math.min(i + win + 1, size); j++) {
                 copies[j] += copies[i];
             }
         }
-
-        int totalCopies = 0;
-        for (int i = 0; i < lines.size(); i++) {
-            System.out.println(copies[i]);
-            totalCopies += copies[i];
-        }
-        return totalCopies;
     }
 
-    public static void partTwo(String puzzleInput) throws IOException {
-        List<String> lines = readFile(puzzleInput);
-        wins = new int[lines.size()];
-        copies = new int[lines.size()];
-        for (int i = 0; i < lines.size(); i++) {
-            copies[i] = 1;
+    public static int sumCopies(int[] copies) {
+        int totalCopies = 0;
+        for (int copy : copies) {
+            totalCopies += copy;
         }
-        int result = parseInput(lines);
-        System.out.println(result);
+        return totalCopies;
     }
 }
