da7a75637fee3fcd2b161e703d2f90bb2644115a
riceisacereal <zhangdorothylele@gmail.com>
2023-12-05 17:42:54 +0100

    Change logic to get rid of hidden iterative element

--- 2023/Day05/PartTwo.java
+++ 2023/Day05/PartTwo.java
@@ -3,15 +3,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 // 393797665 too high
 
 public class PartTwo {
-    private static final String puzzleInput = "2023/Day05/input.txt";
+    private static final String puzzleInput = "2023/Day05/test.txt";
 
     public static void main(String[] args) throws Exception {
         List<String> lines = readFile(puzzleInput);
@@ -34,7 +32,6 @@ public class PartTwo {
         }
 
         ArrayList<long[]> maps = allMaps.get(index);
-        ArrayList<Long> locs = new ArrayList<>();
         for (long[] map : maps) {
             long sourceStart = map[1];
             long dest = map[0];
@@ -44,42 +41,38 @@ public class PartTwo {
 
             // start [ start ] end
             // source ( sourceStart ) sourceEnd
+
+            // O - forward original range
+            // X - calculate destination range
+            // R - redo using same index and range
             if (end <= sourceStart) {
                 // No overlap [O] ()
                 // Use Original range
                 return splitSearch(allMaps, index + 1, start, end);
             } else if (start >= sourceStart && end <= sourceEnd) {
                 // Contained ( [X] )
-                locs.add(splitSearch(allMaps, index + 1, getDest(sourceStart, dest, start), getDest(sourceStart, dest, end)));
-                return Collections.min(locs);
+                return splitSearch(allMaps, index + 1, getDest(sourceStart, dest, start), getDest(sourceStart, dest, end));
             } else if (start < sourceStart && end > sourceEnd) {
-                // Cover [O(X) ]
-                locs.add(splitSearch(allMaps, index + 1, start, sourceStart));
-                locs.add(splitSearch(allMaps, index + 1, getDest(sourceStart, dest, sourceStart), getDest(sourceStart, dest, sourceEnd)));
-                start = sourceEnd;
+                // Cover [O(X)R]
+                long o = splitSearch(allMaps, index + 1, start, sourceStart);
+                long x = splitSearch(allMaps, index + 1, getDest(sourceStart, dest, sourceStart), getDest(sourceStart, dest, sourceEnd));
+                long r = splitSearch(allMaps, index, sourceEnd, end);
+                return Math.min(o, Math.min(x, r));
             } else if (start < sourceStart) {
                 // Second half in [O(X] )
-                // Split
-                locs.add(splitSearch(allMaps, index + 1, start, sourceStart));
-                locs.add(splitSearch(allMaps, index + 1, getDest(sourceStart, dest, sourceStart), getDest(sourceStart, dest, end)));
-                return Collections.min(locs);
+                long o = splitSearch(allMaps, index + 1, start, sourceStart);
+                long x = splitSearch(allMaps, index + 1, getDest(sourceStart, dest, sourceStart), getDest(sourceStart, dest, end));
+                return Math.min(o, x);
             } else if (start < sourceEnd) {
-                // First half in ( [X) ]
-                // Split
-                locs.add(splitSearch(allMaps, index + 1, getDest(sourceStart, dest, start), getDest(sourceStart, dest, sourceEnd)));
-                start = sourceEnd;
-            }
-
-            if (start == end) {
-                break;
+                // First half in ( [X)R]
+                long x = splitSearch(allMaps, index + 1, getDest(sourceStart, dest, start), getDest(sourceStart, dest, sourceEnd));
+                long r = splitSearch(allMaps, index, sourceEnd, end);
+                return Math.min(x, r);
             }
         }
 
         // No overlap () []
-        if (start != end) {
-            return splitSearch(allMaps, index + 1, start, end);
-        }
-        return Collections.min(locs);
+        return splitSearch(allMaps, index + 1, start, end);
     }
 
     public static long parseInput(List<String> lines) throws Exception {
@@ -91,7 +84,7 @@ public class PartTwo {
             long start = seeds[i];
             long end = start + seeds[i + 1];
 
-            // every node returns smallest
+            // Every node returns smallest
             long result = splitSearch(allMaps, 0, start, end);
             locs.add(result);
         }
