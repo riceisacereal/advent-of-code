78afc72a80b4c9c30eccd53494b03c556d60643d
riceisacereal <zhangdorothylele@gmail.com>
2023-12-05 19:55:06 +0100

    Add templates

---
+++ 2023/Day_06/PartTwo.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PartTwo {
+    private static final String puzzleInput = "2023/Day06/test.txt";
+
+}
