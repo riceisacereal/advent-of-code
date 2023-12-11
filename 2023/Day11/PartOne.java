73826b09d00f1826965cd0b06dc7c5d83852e617
riceisacereal <zhangdorothylele@gmail.com>
2023-12-11 10:20:56 +0100

    Clean up to let both parts use the same method

--- 2023/Day11/PartOne.java
+++ 2023/Day11/PartOne.java
@@ -12,26 +12,12 @@ public class PartOne {
 
     public static void main(String[] args) throws IOException {
         List<String> lines = readFile(puzzleInput);
-        long result = partTwo(lines);
+        long result = parseInput(lines);
         System.out.println(result);
     }
 
     public static List<String> readFile(String fileName) throws IOException {
         return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
-    }
-
-    public static boolean allEmptySpace(ArrayList<Character> row) {
-        for (char c : row) {
-            if (c == '#') return false;
-        }
-        return true;
-    }
-
-    public static boolean allEmptySpace(ArrayList<ArrayList<Character>> expandedUniverse, int col) {
-        for (ArrayList<Character> ca : expandedUniverse) {
-            if (ca.get(col) == '#') return false;
-        }
-        return true;
     }
 
     public static boolean allEmptySpace(List<String> lines, int col) {
@@ -41,47 +27,8 @@ public class PartOne {
         return true;
     }
 
-    public static ArrayList<Character> getEmptyRow(int cols) {
-        return new ArrayList<>(Collections.nCopies(cols, '.'));
-    }
-
-    public static ArrayList<ArrayList<Character>> getExpandedUniverse(List<String> lines) {
-        int rows = lines.size();
-        int cols = lines.get(0).length();
-
-        ArrayList<ArrayList<Character>> expandedUniverse = new ArrayList<>();
-        for (String line : lines) {
-            ArrayList<Character> l = new ArrayList<>();
-            for (char c : line.toCharArray()) {
-                l.add(c);
-            }
-            expandedUniverse.add(l);
-        }
-
-        int rowOffset = 0;
-        for (int row = 0; row < rows; row++) {
-            int actualIndex = row + rowOffset;
-            if (allEmptySpace(expandedUniverse.get(actualIndex))) {
-                expandedUniverse.add(actualIndex, getEmptyRow(cols));
-                rowOffset++;
-            }
-        }
-
-        int colOffset = 0;
-        for (int col = 0; col < cols; col++) {
-            int actualIndex = col + colOffset;
-            if (allEmptySpace(expandedUniverse, actualIndex)) {
-                for (ArrayList<Character> ca : expandedUniverse) {
-                    ca.add(actualIndex, '.');
-                }
-                colOffset++;
-            }
-        }
-
-        return expandedUniverse;
-    }
-
-    public static long partTwo(List<String> lines) {
+    public static long parseInput(List<String> lines) {
+        final int EXPAND = 1000000 - 1; // Change this number to toggle between part 1 and 2
         int rows = lines.size();
         int cols = lines.get(0).length();
 
@@ -95,18 +42,16 @@ public class PartOne {
             }
         }
 
-        int expand = 1000000 - 1;
-
         int rowOffset = 0;
         for (int row = 0; row < rows; row++) {
             int newIndex = row + rowOffset;
             if (lines.get(row).replaceAll("#", "").length() == cols) {
                 for (int[] co : galaxyCoordinates) {
                     if (co[0] > newIndex) {
-                        co[0] += expand;
+                        co[0] += EXPAND;
                     }
                 }
-                rowOffset += expand;
+                rowOffset += EXPAND;
             }
         }
 
@@ -116,10 +61,10 @@ public class PartOne {
             if (allEmptySpace(lines, col)) {
                 for (int[] co : galaxyCoordinates) {
                     if (co[1] > newIndex) {
-                        co[1] += expand;
+                        co[1] += EXPAND;
                     }
                 }
-                colOffset += expand;
+                colOffset += EXPAND;
             }
         }
 
@@ -139,40 +84,4 @@ public class PartOne {
 
         return distances;
     }
-
-//    public static int parseInput(List<String> lines) {
-//        ArrayList<ArrayList<Character>> expandedUniverse = getExpandedUniverse(lines);
-//        int rows = expandedUniverse.size();
-//        int cols = expandedUniverse.get(0).size();
-//
-//        ArrayList<int[]> galaxyCoordinates = new ArrayList<>();
-//        for (int row = 0; row < rows; row++) {
-//            for (int col = 0; col < cols; col++) {
-//                char c = expandedUniverse.get(row).get(col);
-//                if (c == '#') {
-//                    galaxyCoordinates.add(new int[] {row, col});
-//                }
-//            }
-//        }
-//
-//        int distances = 0;
-//        for (int n = 0; n < galaxyCoordinates.size(); n++) {
-//            if (n == galaxyCoordinates.size() - 1) {
-//                break;
-//            }
-//
-//            int[] thisGalaxy = galaxyCoordinates.get(n);
-//            for (int next = n + 1; next < galaxyCoordinates.size(); next++) {
-//                int[] nextGalaxy = galaxyCoordinates.get(next);
-//                distances += Math.abs(thisGalaxy[0] - nextGalaxy[0])
-//                    + Math.abs(thisGalaxy[1] - nextGalaxy[1]);
-//            }
-//        }
-//
-//        for (String line : lines) {
-//            System.out.println(line);
-//        }
-//
-//        return distances;
-//    }
 }
