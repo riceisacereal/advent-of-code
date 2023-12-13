e387283980455125d6aefeefc3eeba261b6ca9eb
riceisacereal <zhangdorothylele@gmail.com>
2023-12-13 21:54:21 +0100

    2023.13.1&2 2 was blazingly fast

---
+++ 2023/Day13/Main.java
@@ -0,0 +1,94 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private static final String puzzleInput = "2023/Day13/input.txt";
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = readFile(puzzleInput);
+        int result = parseInput(lines);
+        System.out.println(result);
+    }
+
+    public static List<String> readFile(String fileName) throws IOException {
+        return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+    }
+
+    public static int checkReflection(ArrayList<ArrayList<char[]>> allPatterns) {
+        int result = 0;
+
+        for (ArrayList<char[]> pattern : allPatterns) {
+            int maxY = pattern.size();
+            int maxX = pattern.get(0).length;
+
+            HashMap<String, Integer> count = new HashMap<>();
+            for (int i = 0; i < maxX; i++) {
+                for (int j = 0; j < maxY; j++) {
+                    // check left-right reflection
+                    int left = i;
+                    int right = i + 1;
+                    count.merge("col" + i, 1, Integer::sum);
+                    while (left >= 0 && right < maxX) {
+                        if (pattern.get(j)[left] != pattern.get(j)[right]) {
+                            count.put("col" + i, count.get("col" + i) - 1);
+                            break;
+                        }
+                        left--;
+                        right++;
+                    }
+
+                    // check up-down reflection when not last row
+                    int up = j;
+                    int down = j + 1;
+                    count.merge("row" + j, 1, Integer::sum);
+                    while (up >= 0 && down < maxY) {
+                        if (pattern.get(up)[i] != pattern.get(down)[i]) {
+                            count.put("row" + j, count.get("row" + j) - 1);
+                            break;
+                        }
+                        up--;
+                        down++;
+                    }
+                }
+            }
+
+            // check all row axis
+            for (int i = 0; i < maxY - 1; i++) {
+                if (count.get("row" + i) == maxX - 1) { // Remove -1 for part 1 answer
+                    result += 100 *(i + 1);
+                }
+            }
+
+            // check all column axis
+            for (int i = 0; i < maxX - 1; i++) {
+                if (count.get("col" + i) == maxY - 1) { // Remove -1 for part 1 answer
+                    result += i + 1;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static int parseInput(List<String> lines) {
+        ArrayList<ArrayList<char[]>> allPatterns = new ArrayList<>();
+        ArrayList<char[]> pattern = new ArrayList<>();
+        for (String line : lines) {
+            if (line.isBlank()) { // If line is only new line
+                allPatterns.add(pattern);
+                pattern = new ArrayList<>();
+                continue;
+            }
+            pattern.add(line.toCharArray());
+        }
+        allPatterns.add(pattern);
+
+        return checkReflection(allPatterns);
+    }
+}
