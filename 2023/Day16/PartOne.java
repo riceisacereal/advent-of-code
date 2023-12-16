c2213fe5c175d502d47feb1aea9ff706ae58dd63
riceisacereal <zhangdorothylele@gmail.com>
2023-12-16 14:00:34 +0100

    Remove unused import

--- 2023/Day16/PartOne.java
+++ 2023/Day16/PartOne.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class PartOne {
     private static final String puzzleInput = "2023/Day16/input.txt";
