b0d13354a0b952f28381a5df49db4bfed3aef4c0
riceisacereal <zhangdorothylele@gmail.com>
2024-01-04 13:00:35 +0100

    Many failed attempts

--- 2023/Day24/PartTwo.java
+++ 2023/Day24/PartTwo.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class PartTwo {
@@ -20,67 +21,13 @@ public class PartTwo {
         return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
     }
 
-//    private static long findTime(ArrayList<Hail> hails) {
-//        ArrayList<long[]> hail = new ArrayList<>();
-//        for (Hail h : hails) {
-//            hail.add(new long[] {h.loc[0], h.vel[0]});
-//        }
-//
-//        long i = 0;
-//        while (true) {
-//            hail.sort(Comparator.comparingLong(a -> a[0]));
-//            long vel = (hail.get(1)[0] - hail.get(0)[0]) / (hail.get(1)[1] - hail.get(0)[1]);
-//            i++;
-//        }
-//        return i;
-//    }
-
-//    public static void checkParallel(Hail h1, Hail h2) {
-//        double a = (double) h1.vel[0] / (double) h2.vel[0];
-//        double b = (double) h1.vel[1] / (double) h2.vel[1];
-//        double c = (double) h1.vel[2] / (double) h2.vel[2];
-//        if (Math.abs(a - b)< 0.0000001){
-//            System.out.println("Found parallel");
-//        }
-//    }
-
-    public static void getValues() {
-        int a, b, c, x, y, z, A, B, C, D, E, F, G, H;
-        // 19, 13, 30 @ -2, 1, -2
-        // 18, 19, 22 @ -1, -1, -2 //A18 B-1 C19 D-1
-        // 20, 25, 34 @ -2, -2, -4 //
-        // 12, 31, 28 @ -1, -2, -1
-        // 20, 19, 15 @ 1, -5, -3
-
-        // 20, 19, 15 @  1, -5, -3
-        a = 24;
-        b = 13;
-        c = 10;
-        x = -3;
-        y = 1;
-        z = 2;
-//        A = 19;
-//        B = -2;
-//        C = 13;
-//        D = 1;
-        A = 20;
-        B = 1;
-        C = 19;
-        D = -5;
-
-        E = 18;
-        F = -1;
-        G = 19;
-        H = -1;
-
-        double stage1 = (double) (B * C - A * D + a * D - B * b + A * y - a * y) / (C - b);
-        double stage2 = (F * b * C - F * Math.pow(b, 2) + G * B * C - A * D * G + D * G * a - B * G * b
-            - F * G * C + F * G * b + H * E * C - H * E * b - H * a * C + H * a * b - B * C * b +
-            A * D * b - D * a * b + B * Math.pow(b, 2))
-            / (A * b + E * C - E * b - a * C - A * G + G * a);
-
-        System.out.println(stage1);
-        System.out.println(stage2);
+    public static boolean checkOrder(ArrayList<Hail> initialOrder, ArrayList<Hail> hails) {
+        for (int i = 0; i < hails.size(); i++) {
+            if (initialOrder.get(i) != hails.get(i)) {
+                 return false;
+            }
+        }
+        return true;
     }
 
     public static int parseInput(List<String> lines) {
@@ -90,6 +37,59 @@ public class PartTwo {
             hails.add(new Hail(line));
         }
 
+        // Remove ones with the same coefficient
+//        HashSet<Integer> xCoefficient = new HashSet<>();
+//        int lastIndex = 0;
+//        boolean removed = true;
+//        while (removed) {
+//            removed = false;
+//            for (int i = lastIndex; i < hails.size(); i++) {
+//                Hail h = hails.get(i);
+//                if (xCoefficient.contains(h.vel[0])) {
+//                    hails.remove(h);
+//                    lastIndex = i;
+//                    removed = true;
+//                    break;
+//                } else {
+//                    xCoefficient.add(h.vel[0]);
+//                }
+//            }
+//        }
+
+        for (Hail h : hails) {
+            h.loc[0] += 100L * h.vel[0];
+        }
+        hails.sort(Comparator.comparingLong(a -> a.loc[0]));
+        ArrayList<Hail> initialOrder = new ArrayList<>(hails);
+        ArrayList<Hail> reverseOrder = new ArrayList<>(hails);
+        Collections.reverse(reverseOrder);
+
+        // Check order for one axis
+        long before = 0;
+        long after = 0;
+        long t = 1000000;
+//        while (true) {
+//            for (Hail h : hails) {
+//                h.loc[0] += h.vel[0];
+//            }
+//            hails.sort(Comparator.comparingLong(a -> a.loc[0]));
+//            if (checkOrder(initialOrder, hails)) {
+//                before = t;
+//            } else {
+//                System.out.println(before + " " +  t);
+//                break;
+//            }
+////            } else if (checkOrder(reverseOrder, hails)) {
+////                after = t;
+////                break;
+////            }
+//            t++;
+//        }
+
+        System.out.println(108375683349444L + 220656145109505L + 289502736377988L);
+        // 11165357 is when order changes
+        System.out.println(before + " " + after);
+
         // For every time t >= 0
         // sort on x axis
 //        int points = 0;
@@ -98,7 +98,6 @@ public class PartTwo {
 ////                checkParallel(hails.get(i), hails.get(j));
 //            }
 //        }
-        getValues();
         return 0;
     }
 }
