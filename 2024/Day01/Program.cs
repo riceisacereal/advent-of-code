933b65c695e03788b4309011b72d589d67c4f2e4
riceisacereal <zhangdorothylele@gmail.com>
2024-11-29 13:19:51 +0100

    Setup

---
+++ 2024/Day01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      String line;
+      try
+      {
+        StreamReader sr = new StreamReader("../../../test.txt");
+        line = sr.ReadLine();
+
+        while (line != null)
+        {
+          Console.WriteLine(line);
+          line = sr.ReadLine();
+        }
+
+        sr.Close();
+        Console.ReadLine();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Exception: " + e.Message);
+      }
+    }
+  }
+}
